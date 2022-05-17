a28d1f87696debb56b130a9c5a00d6502bebc0f2
bastien-mva <bastien.batardiere@gmail.com>
2022-05-17 15:07:53 +0200

    Implemented the Sandwich estimator for Theta. Works fine (about 90% of the parameters inside the 95% confidence interval.

--- VEM.r
+++ VEM.r
@@ -1,7 +1,8 @@
 library(torch)
 library(R6)
+setwd("~/Documents/These/R_PLN")
 source(file = 'utils.r')
-setwd("~/Documents/R_PLN")
+source(file = 'IMPS_PLN.r')
 
 
 
@@ -11,7 +12,6 @@ covariates <- torch_tensor(as.matrix(read.csv('covariates.csv')))
 true_Sigma <- torch_tensor(as.matrix(read.csv('true_5_Sigma.csv')))
 #true_C <- torch_cholesky(true_Sigma)
 true_Theta <- torch_tensor(as.matrix(read.csv('true_beta.csv')))
-
 #Variational parameters 
 
 
@@ -54,7 +54,6 @@ VEM_PLN <- R6Class("VEM_PLN",
                        self$Y <- Y
                        self$O <- O
                        self$covariates <- covariates
-                       pr('covariates', self$covariates)
                        self$p <- Y$shape[2]
                        self$n <- Y$shape[1]
                        self$d <- covariates$shape[2]
@@ -66,7 +65,7 @@ VEM_PLN <- R6Class("VEM_PLN",
                        self$Sigma <- torch_eye(self$p)
                      },
                      
-                     fit = function(N_iter, lr){
+                     fit = function(N_iter, lr, verbose = FALSE){
                        optimizer = optim_rprop(c(self$Theta, self$M, self$S), lr = lr)
                        for (i in 1:N_iter){
                          optimizer$zero_grad()
@@ -75,19 +74,19 @@ VEM_PLN <- R6Class("VEM_PLN",
                          loss$backward()
                          optimizer$step()
                          self$Sigma <- first_closed_Sigma(self$M, self$S)
-                         pr('ELBO', -loss$item()/(self$n))
-                         pr('MSE Sigma', MSE(self$Sigma - true_Sigma))
-                         pr('MSE Theta', MSE(self$Theta- true_Theta))
+                         if(verbose){
+                           pr('ELBO', -loss$item()/(self$n))
+                           pr('MSE Sigma', MSE(self$Sigma - true_Sigma))
+                           pr('MSE Theta', MSE(self$Theta- true_Theta))
+                         }
+                         
                        }
                      }, 
                      
                      get_Dn_Theta = function(){
-                       
                        YmoinsA = self$Y - self$A 
                        outer_prod_YmoinsA = torch_matmul(YmoinsA$unsqueeze(3), YmoinsA$unsqueeze(2))
                        outer_prod_X = torch_matmul(self$covariates$unsqueeze(3), self$covariates$unsqueeze(2))
-                       pr('outer prod shape', outer_prod_YmoinsA$shape)
-                       pr('outer_prod X shape', outer_prod_X$shape)
                        size_kron = outer_prod_YmoinsA$shape[2]*outer_prod_X$shape[2]
                        res = torch_zeros(size_kron, size_kron)
                        for ( i in 1:self$n){
@@ -95,32 +94,102 @@ VEM_PLN <- R6Class("VEM_PLN",
                        }
                        return(res/self$n)
                        },
-                     get_Cn_Theta = function(){
-                       
-                       for (i in 1:n){
-                         C_i = torch_diag(2/(2+self$S[i,]^2*self$A[i,]))
-                         
-                       }
-                     },
+
                     get_mat_i_Theta = function(i){
                       D_i = torch_diag(self$A[i,])
-                      C_i = torch_diag(2/(2+self$S[i,]^2*self$A[i,]))
+                      C_i = torch_diag(2/(2+self$S[i,]^4*self$A[i,]))
+                      C_i_inv =torch_diag((2+self$S[i,]^4*self$A[i,])/2)
                       D_i_inv_sqrt = torch_diag(self$A[i,]**(-1/2))
+                      D_i_sqrt = torch_diag(self$A[i,]**(1/2))
                       invB_i = C_i + torch_matmul(torch_matmul(D_i_inv_sqrt, self$Sigma), D_i_inv_sqrt)
-                      B_i = torch_inv(invB_i)
+                      B_i = torch_inverse(invB_i)
+                      Big_mat = torch_mm(torch_mm(torch_mm(torch_mm(D_i_sqrt, C_i), C_i_inv -B_i),C_i), D_i_sqrt)  
+                      return(Big_mat)
+                      },
+                    get_Cn_Theta = function(){
+                      C_n = torch_zeros(self$d*self$p, self$d*self$p)
+                      for (i in 1:(self$n)){
+                        big_mat = self$get_mat_i_Theta(i)
+                        x_i <-  self$covariates[i,]
+                        xxt = torch_matmul(x_i$unsqueeze(2), x_i$unsqueeze(1))
+                        C_n = C_n + torch_kron(big_mat, xxt)
                       }
+                      return(-C_n/(self$n))
+                    },
+                    get_variational_inv_Fisher = function(){
+                      Dn = self$get_Dn_Theta()
+                      Cn = self$get_Cn_Theta()
+                      inv_Dn = torch_inverse(Dn)
+                      return(torch_mm(torch_mm(Cn, inv_Dn), Cn))
+                    }
                      
                    )
                    )
 
+d = 3L
+n = 500L
+p = 10L
+q = p
 
+### Sampling some data ###
+O <-  torch_tensor(matrix(0,nrow = n, ncol = p))
+covariates <- torch_tensor(matrix(rnorm(n*d),nrow = n, ncol = d))
+true_Theta <- torch_tensor(matrix(rnorm(d*p),nrow = d, ncol = p))
+true_C <- torch_tensor(matrix(rnorm(p*q), nrow = p, ncol = q) )/3
+true_Sigma <- torch_matmul(true_C,torch_transpose(true_C, 2,1))
+true_Theta <- torch_tensor(matrix(rnorm(d*p),nrow = d, ncol = p))/2
+true_C <- C_from_Sigma(true_Sigma,q)
+Y <- sample_PLN(true_C,true_Theta,O,covariates)
+
+nb_iter = 15
+var_percentage = torch_zeros(nb_iter)
+imps_percentage = torch_zeros(nb_iter)
+
+for ( i in 1:nb_iter){
+  Y <- sample_PLN(true_C,true_Theta,O,covariates)
+  pln = VEM_PLN$new(Y,O,covariates)
+  pln$fit(2,0.1, verbose = TRUE)
+  inv_Fischer = pln$get_variational_inv_Fisher()
+  res_var = how_much(pln$Theta, true_Theta, inv_Fischer, p*d-5L,Y$shape[1])
+  var_percentage[i] = res_var 
+  pr('res_var', res_var)
+  imps <- IMPS_PLN$new(Y,O,covariates,q)
+  imps$C <- torch_clone(true_C) #torch_clone(true_C) + 0*torch_randn(true_C$shape)
+  imps$Theta <- pln$Theta#true_Theta + 0.4*torch_randn(true_Theta$shape)
+  imps$fit(1L, acc = 0.008, lr = 0)
+  hess_log_p_theta <- imps$compute_hess_log_p_theta()
+  res_imps = how_much(imps$Theta, true_Theta, -hess_log_p_theta, p*d - 5L, Y$shape[1]) 
+  pr('res_imps', res_imps)
+  imps_percentage[i] = res_imps
+}
+
+pr('mean var', mean_var)
+pr('mean imps', mean_imps/nb_iter)
 
 pln = VEM_PLN$new(Y,O,covariates)
-pln$fit(10,0.1)
+pln$fit(900,0.1)
+inv_Fischer = pln$get_variational_inv_Fisher()
+how_much(pln$Theta, true_Theta, inv_Fischer, p*d-5L,Y$shape[1])
 
-pln$get_mat_i_Theta(1)
 
 
+imps <- IMPS_PLN$new(Y,O,covariates,q)
+imps$C <- torch_clone(true_C) #torch_clone(true_C) + 0*torch_randn(true_C$shape)
+imps$Theta <- pln$Theta#true_Theta + 0.4*torch_randn(true_Theta$shape)
+imps$fit(1L, acc = 0.008, lr = 0)
+
+hess_log_p_theta <- imps$compute_hess_log_p_theta()
+how_much(imps$Theta, true_Theta, -hess_log_p_theta, p*d - 5L, Y$shape[1] )
+
+
+
+
+
+torch_cholesky(inv_Fischer)
+pln$get_Cn_Theta()
+
+
+#verif si on a bien la meme chose avec les deux formules (vectorisées ou non) de Kronecker
 pln$A
 torch_diag(2/(2+self$S[i,]^2*self$A[i,]))
 pln$get_Dn_Theta()
