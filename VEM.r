6a1ff3c3be3a3d726ca05155b388d901dc540f7b
bastien-mva <bastien.batardiere@gmail.com>
2023-02-24 15:04:21 +0100

    Commited some old modifications.

--- VEM.r
+++ VEM.r
@@ -1,8 +1,6 @@
 library(torch)
 library(R6)
-setwd("~/Documents/R_PLN")
-source(file = 'utils.r')
-
+source(file = '../R_PLN/utils.r')
 # 
 # 
 # Y <- torch_tensor(as.matrix(read.csv('Y.csv')))
@@ -33,6 +31,61 @@ ELBO <- function(Y, O, covariates, M, S, Sigma, Theta){
   return(tmp)
 }
 
+fastELBO <-  function(Y, O, covariates, M, S, Sigma, beta){
+    ###Compute the ELBO (Evidence LOwer Bound. See the doc for more details
+    #on the computation.
+
+    #Args:
+    #    Y: torch.tensor. Counts with size (n,p)
+    #    0: torch.tensor. Offset, size (n,p)
+    #    covariates: torch.tensor. Covariates, size (n,d)
+    #    M: torch.tensor. Variational parameter with size (n,p)
+    #    S: torch.tensor. Variational parameter with size (n,p)
+    #    Sigma: torch.tensor. Model parameter with size (p,p)
+    #    beta: torch.tensor. Model parameter with size (d,p)
+    #Returns:
+    #    torch.tensor of size 1 with a gradient. The ELBO.
+    ###
+    n <- Y$shape[1] 
+    p <- Y$shape[2]
+    SrondS <- torch_multiply(S, S)
+    OplusM <- O + M
+    MmoinsXB <- M - torch_mm(covariates, beta)
+    
+    tmp <- - n / 2 * torch_logdet(Sigma)
+    tmp = tmp + torch_sum(torch_multiply(Y, OplusM)
+                    - torch_exp(OplusM + SrondS / 2)
+                    + 1 / 2 * torch_log(SrondS)
+                    )
+    DplusMmoinsXB2 = torch_diag(
+        torch_sum(SrondS, dim=1)) + torch_mm(MmoinsXB$t(), MmoinsXB)
+    tmp = tmp -1 / 2 * torch_trace(
+        torch_mm(
+            torch_inverse(Sigma),
+            DplusMmoinsXB2
+        )
+    )
+    tmp = tmp-torch_sum(log_stirling(Y))
+    tmp = tmp + n * p / 2 
+    return(tmp)
+}
+
+fastClosedSigma <- function(covariates, M, S, beta){
+        ### Closed form for Sigma for the M step.
+        n = M$shape[1]
+        p = M$shape[2]
+        MmoinsXB = M - torch_mm(covariates, beta)
+        closed = torch_mm(MmoinsXB$t(), MmoinsXB)
+        closed = closed + torch_diag(torch_sum(torch_multiply(S, S), dim=1))
+        return (1 / (n) * closed)
+    }
+
+fastClosedTheta<- function(covariates, M){
+    ##Closed form for beta for the M step
+    return(torch_mm(torch_mm(torch_inverse(torch_mm(covariates$t(),covariates)),covariates$t()),M))
+    }
+
+
 
 
 VEM_PLN <- R6Class("VEM_PLN", 
@@ -49,34 +102,62 @@ VEM_PLN <- R6Class("VEM_PLN",
                      Sigma = NULL, 
                      Theta = NULL,
                      ELBO_list = NULL, 
-                     initialize = function(Y,O,covariates){
+                     goodInit = NULL, 
+                     fast = NULL,
+                     initialize = function(Y,O,covariates, goodInit = TRUE, fast = TRUE){
                        self$Y <- Y
                        self$O <- O
                        self$covariates <- covariates
                        self$p <- Y$shape[2]
                        self$n <- Y$shape[1]
                        self$d <- covariates$shape[2]
+                       self$goodInit = goodInit
+                       self$fast = fast
+                       print('Initialization...')
+                       ## Model parameters 
+                       if (self$goodInit) {
+                           self$Theta <- Poisson_reg(Y,O,covariates)$detach()$clone()$requires_grad_(TRUE)
+                           self$Sigma <- init_Sigma(self$Y, self$O, self$covariates,self$Theta)$detach()$clone()
+                       }
+                       else{
+                            self$Theta <- torch_zeros(self$d,self$p, requires_grad = TRUE)
+                            self$Sigma <- torch_eye(self$p)
+                       }
                        ## Variational parameters
                        self$M <- torch_zeros(self$n, self$p, requires_grad = TRUE)
                        self$S <- torch_ones(self$n, self$p, requires_grad = TRUE)
-                       ## Model parameters 
-                       self$Theta <- torch_zeros(self$d,self$p, requires_grad = TRUE)
-                       self$Sigma <- torch_eye(self$p)
+                       print('Initialization finished.')
                        self$ELBO_list = c()
                      },
                      
                      fit = function(N_iter, lr, verbose = FALSE){
-                       optimizer = optim_rprop(c(self$Theta, self$M, self$S), lr = lr)
+                       if (self$fast) {
+                           optimizer = optim_rprop(c(self$M, self$S), lr = lr)
+                       }
+                       else{
+                           optimizer = optim_rprop(c(self$Theta, self$M, self$S), lr = lr)
+                       }
                        for (i in 1:N_iter){
                          optimizer$zero_grad()
-                         self$A = torch_exp(self$O + torch_matmul(self$covariates,self$Theta) + self$M + 1/2*(self$S)**2)
-                         loss = - ELBO(self$Y, self$O, self$covariates, self$M, self$S, self$Sigma, self$Theta)
-                         loss$backward()
-                         optimizer$step()
-                         self$Sigma <- first_closed_Sigma(self$M, self$S)
+                         
+                         if(self$fast){
+                            loss = - fastELBO(self$Y, self$O, self$covariates, self$M, self$S, self$Sigma, self$Theta)
+                            loss$backward()
+                            optimizer$step()
+                            self$Theta <- fastClosedTheta(self$covariates, self$M)
+                            self$Sigma <- fastClosedSigma(self$covariates, self$M, self$S, self$Theta)
+                             }
+                         else{
+                             self$A = torch_exp(self$O + torch_matmul(self$covariates,self$Theta) + self$M + 1/2*(self$S)**2)
+                             loss = - ELBO(self$Y, self$O, self$covariates, self$M, self$S, self$Sigma, self$Theta)
+                             loss$backward()
+                             optimizer$step()
+                             self$Sigma <- first_closed_Sigma(self$M, self$S)
+                         }
+                         
                          if(verbose && (i %% 50 == 0)){
-                           pr('i : ', i )
-                           pr('ELBO', -loss$item()/(self$n))
+                           message('i : ', as.character(i))
+                           message('ELBO : ', as.character(-loss$item()/(self$n)))
                            #pr('MSE Sigma', MSE(self$Sigma - true_Sigma))
                            #pr('MSE Theta', MSE(self$Theta- true_Theta))
                          }
@@ -126,8 +207,26 @@ VEM_PLN <- R6Class("VEM_PLN",
                       Cn = self$get_Cn_Theta()
                       inv_Dn = torch_inverse(Dn)
                       return(torch_mm(torch_mm(Cn, inv_Dn), Cn))
-                    }
-                     
+                    },
+                    getLatentVariables = function(take_cov = FALSE){
+                       if (self$fast){
+                           if (take_cov){
+                               return(self$M)
+                               }
+                           else{
+                               return(self$M - torch_mm(self$covariates, self$Theta))
+                               }
+                           }
+                       else{
+                           if (take_cov){
+                               return(self$M + torch_mm(self$covariates, self$Theta))
+                               }
+                           else{
+                               return(self$M)
+                               }
+                           }
+                        }
+
                    )
                    )
 
@@ -180,7 +279,7 @@ VEM_PLNPCA <- R6Class("VEM_PLNPCA",
                        self$M <- torch_zeros(self$n, self$q, requires_grad = TRUE)
                        self$S <- torch_ones(self$n, self$q, requires_grad = TRUE)
                        ## Model parameters 
-                       print('Initialization ...')
+                       message('Initialization ...')
                        if (self$good_init){
                           self$Theta <- Poisson_reg(Y,O,covariates)$detach()$clone()$requires_grad_(TRUE)
                           self$C <- init_C(Y,O,covariates, self$Theta,q)$detach()$clone()$requires_grad_(TRUE)
@@ -189,8 +288,7 @@ VEM_PLNPCA <- R6Class("VEM_PLNPCA",
                          self$Theta <- torch_zeros(self$d, self$p, requires_grad = TRUE)
                          self$C <- torch_randn(self$p, self$q, requires_grad = TRUE)
                        }
-                       print('Initialization finished.')
-                       self$Sigma <- torch_eye(self$p)
+                       message('Initialization finished.')
                        self$fitted = FALSE
                        self$ELBO_list = c()
                      },
@@ -204,13 +302,12 @@ VEM_PLNPCA <- R6Class("VEM_PLNPCA",
                          #self$A = torch_exp(self$O + torch_matmul(self$covariates,self$Theta) + self$M + 1/2*(self$S)**2)
                          loss = - ELBO_PCA(self$Y, self$O, self$covariates, self$M, self$S, self$C, self$Theta)
                          loss$backward()
-                         #pr('grad C', self$C$grad)
                          optimizer$step()
                          #self$Sigma <- first_closed_Sigma(self$M, self$S)
                          if(verbose && (i%%50 == 0)){
-                           pr('i :', i )
-                           pr('norm sig', torch_norm(self$get_Sigma()))
-                           pr('ELBO', -loss$item()/(self$n))
+                           message('i :', as.character(i) )
+                           message('norm sig', as.character(torch_norm(self$get_Sigma())$item()))
+                           message('ELBO : ', as.character(-loss$item()/(self$n)))
                            #pr('MSE Sigma', MSE(self$get_Sigma() - true_Sigma))
                            #pr('MSE Theta', MSE(self$Theta- true_Theta))
                          }
@@ -220,7 +317,20 @@ VEM_PLNPCA <- R6Class("VEM_PLNPCA",
                      }, 
                   plot_log_neg_ELBO = function(from = 10){
                     plot(log(-self$ELBO_list[from:length(self$ELBO_list) ]))
-                  }
+                  },
+                  getLatentVariables = function(take_cov = FALSE){
+                        if (take_cov){
+                            return( torch_matmul(self$M$unsqueeze(2),self$C$t()$unsqueeze(1))$squeeze()+  torch_mm(self$covariates, self$Theta))
+                           }
+                        else{
+                             return(torch_matmul(self$M$unsqueeze(2),self$C$t()$unsqueeze(1))$squeeze())
+                             }
+                    },
+                    getPostSigma = function(){
+                       #pr('first', torch_matmul(self$M$t(),self$M) +torch_diag(torch_sum(torch_multiply(self$S,self$S), dim = 1)))
+                       #pr('sec', 
+                        return(torch_matmul(torch_matmul(self$C, 1/self$n*(torch_matmul(self$M$t(),self$M) +torch_diag(torch_sum(torch_multiply(self$S,self$S), dim = 1)))), self$C$t()))
+                       }
                    )
 )
 
--- sandwich_estimation.R
+++ sandwich_estimation.R
@@ -14,6 +14,7 @@ q = 10L
 
 
 
+
 ### Sampling some data ###
 O <-  torch_tensor(matrix(0,nrow = n, ncol = p))
 covariates <- torch_tensor(matrix(rnorm(n*d),nrow = n, ncol = d))
@@ -22,6 +23,7 @@ true_C <- torch_tensor(matrix(rnorm(p*q), nrow = p, ncol = q) )/3
 true_Sigma <- torch_matmul(true_C,torch_transpose(true_C, 2,1))
 true_Theta <- torch_tensor(matrix(rnorm(d*p),nrow = d, ncol = p))/2
 true_C <- C_from_Sigma(true_Sigma,q)
+true_B_zero <- torch_randn(n,p)
 Y <- sample_PLN(true_C,true_Theta,O,covariates)
 n_a = 200
 n_b = 300
@@ -36,14 +38,14 @@ for ( i in 1:nb_iter){
   pln$fit(2,0.1, verbose = TRUE)
   inv_Fischer = pln$get_variational_inv_Fisher()
   res_var = how_much(pln$Theta, true_Theta, inv_Fischer, p*d-5L,Y$shape[1])
-  var_percentage[i] = res_var 
+  var_percentage[i] = res_var
   pr('res_var', res_var)
   imps <- IMPS_PLN$new(Y,O,covariates,q)
   imps$C <- torch_clone(true_C) #torch_clone(true_C) + 0*torch_randn(true_C$shape)
   imps$Theta <- pln$Theta#true_Theta + 0.4*torch_randn(true_Theta$shape)
   imps$fit(1L, acc = 0.008, lr = 0)
   hess_log_p_theta <- imps$compute_hess_log_p_theta()
-  res_imps = how_much(imps$Theta, true_Theta, -hess_log_p_theta, p*d - 5L, Y$shape[1]) 
+  res_imps = how_much(imps$Theta, true_Theta, -hess_log_p_theta, p*d - 5L, Y$shape[1])
   pr('res_imps', res_imps)
   imps_percentage[i] = res_imps
 }
